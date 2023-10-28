11b5dfb691fe1b59972c80378e12064428e178ca
Evan <543357597@qq.com>
2023-10-28 18:14:56 +0800

    update

--- run_build.sh
+++
@@ -1,3 +0,0 @@
-docker run -it --rm -v /ifs:/ifs -p 8090:8090 jekyll:website
-cd /ifs/www/website/
-jekyll build --trace
--- run_update_site.sh
+++
@@ -1,7 +0,0 @@
-rm _site/run_build.sh
-rm _site/run_serve.sh
-
-cd _site
-git add .
-git commit -m 'update'
-git push
